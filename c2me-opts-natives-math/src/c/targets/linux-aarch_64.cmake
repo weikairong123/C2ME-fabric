81832e4fe1c626e184a820fe1e3752c0101b4388
weikairong123 <19322558991@163.com>
2026-08-21 13:43:41 +0800

    Update linux-aarch_64.cmake

--- c2me-opts-natives-math/src/c/targets/linux-aarch_64.cmake
+++ c2me-opts-natives-math/src/c/targets/linux-aarch_64.cmake
@@ -1,3 +1,7 @@
+
+
+
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm64)
 
@@ -8,3 +12,4 @@ set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
+set(CMAKE_C_FLAGS -march=aarch64)
